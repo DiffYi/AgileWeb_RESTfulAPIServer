9d0c4bc84ba498f062391815a5fd0bd61efd3689
DiffYi <38909816+DiffYi@users.noreply.github.com>
2018-05-16 17:11:28 +0800

    Update todoListRepository.js

--- app/repositories/todoListRepository.js
+++ app/repositories/todoListRepository.js
@@ -32,11 +32,21 @@ class TodoListRepository {
 
   createTodo(todoBody) {
     //实现创建新todo纪录的方法
-    
+    this.todoList.push(todoBody);
   }
 
   updateTodo(id, update) {
     //实现通过id和一个更新对象来更新todo纪录的方法
+    for(var i = 0;i<this.todoList.length;i++)
+    {
+        if(this.todoList[i].id == id)
+        {
+            this.todoList.splice(i,1);
+            this.todoList.push(update);
+            return "Update ID:" + id;
+        }          
+    }
+    return "No This ID:" + id;
   }
 
   deleteTodoBy(id) {
